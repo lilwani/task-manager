b1e0e3a4ca6c31b5714885e79ef17da5de8ea8a2
Lilwani <yashlilwani18@gmail.com>
2021-03-28 15:29:56 +0530

    Add login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { TaskViewComponent } from "./pages/task-view/task-view.component";
 
 const routes: Routes = [
-   {path: '', component: TaskViewComponent}
+   {path: '', component: LoginPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+// import { fontawesome } from "../../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskViewComponent
+    TaskViewComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
