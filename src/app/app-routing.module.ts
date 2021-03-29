a6e0fe6d3b3d6f869ced6b4692734f188e98d703
Lilwani <yashlilwani18@gmail.com>
2021-03-29 18:22:17 +0530

    Add task view path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { TaskViewComponent } from "./pages/task-view/task-view.component";
 
 const routes: Routes = [
-   {path: '', component: LoginPageComponent}
+   {path: '', component: LoginPageComponent},
+   {path: 'taskview', component:TaskViewComponent}
 ];
 
 @NgModule({
